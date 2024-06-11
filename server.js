c5533d64ef33863749a3538bb21a3a75cbc6c2b2
Harshit Daga <121958665+HarshitD-003@users.noreply.github.com>
2024-06-11 14:17:37 +0530

    Config

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://irr-seven.vercel.app',
     methods: ['GET', 'POST'],// Adjust the allowed methods as needed
     allowedHeaders: ['Content-Type'], // Adjust the allowed headers as needed
 }));
